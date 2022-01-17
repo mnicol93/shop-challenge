28a55894dbce8a8f129b9ac4600be9b73ca1f5b8
marcno <mnicolas-oliva@myseneca.ca>
2022-01-17 18:23:17 -0500

    Epic updated

--- src/components/Epic-Viejo.js
+++ src/components/Epic-Viejo.js
@@ -10,26 +10,24 @@ import LikeBtn from "./Shared/Like/LikeBtn";
 
 import "react-datepicker/dist/react-datepicker.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { render } from "@testing-library/react";
 
 //TODO: If browsing on mobile, make picture fetched be .jpg, if it is a browser .png
 //      https://stackoverflow.com/questions/39435395/reactjs-how-to-determine-if-the-application-is-being-viewed-on-mobile-or-deskto
-//      
-//      Render every time datepicker is updated
-//      https://reactjs.org/docs/state-and-lifecycle.html
 
 function Epic() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
+  const [itemsDate, setItemsDate] = useState([]);
+  const [dateExist, setDateExist] = useState(false);
   const [itemsImg, setItemsImg] = useState([]);
-  const [startDate, setStartDate] = useState(new Date('2019-05-30'));
+  const [startDate, setStartDate] = useState(new Date());
   const [year, setYear] = useState();
   const [month, setMonth] = useState();
   const [day, setDay] = useState();
 
-
-  const apiKey = "?api_key=KlkDfF9JFLyn7nS1iwrO5hDtOX3vvghSZeGWLpgd";
-  const apiImg = `https://api.nasa.gov/EPIC/api/natural/date/`;
+  const apiImg = "https://api.nasa.gov/EPIC/api/natural/images?";
+  const apiDates = "https://api.nasa.gov/EPIC/api/natural/all?";
+  const apiKey = "api_key=KlkDfF9JFLyn7nS1iwrO5hDtOX3vvghSZeGWLpgd";
 
   let format = isMobile === true ? "jpg" : "png"; //Will load picture format depending on amount of data available.
 
@@ -43,26 +41,28 @@ function Epic() {
   };
 
   useEffect(() => {
-    fetch(`${apiImg}${startDate.toISOString().split("T")[0]}${apiKey}`)
-        .then((res) => res.json())
-        .then((result) => {
+    fetch(`${apiDates}${apiKey}`)
+      .then((res) => res.json())
+      .then((result) => {
         setIsLoaded(true);
-        setItemsImg(result);
-        console.log(`${apiImg}${startDate.toISOString().split("T")[0]}${apiKey}`);
-        console.log(result);
-        },
-    (error) => {
-        setIsLoaded(true);
-        setError(error);
-    }
-        )
+        setItemsDate(result);
+      })
+      .then(
+        fetch(`${apiImg}${apiKey}`)
+          .then((res) => res.json())
+          .then((result) => {
+            setIsLoaded(true);
+            setItemsImg(result);
+          }),
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
       .catch((err) => {
         console.log(err);
       });
-  }, [apiImg]);
-
-  console.log(itemsImg);
-
+  }, []);
   if (error) {
     return <div>Error: {error.message}</div>;
   } else if (!isLoaded) {
@@ -72,32 +72,19 @@ function Epic() {
         <p class="loading-text">LOADING...</p>
       </div>
     );
-
-    
   }
   return (
     <>
       <DatePicker selected={startDate} onChange={handleChange} dateFormat="yyyy-MM-dd" />
-        <p>{itemsImg.map}</p>
-      {itemsImg.map((item)=>{
-          return(
-            <Container>
-                <Card>
-                <Card.Body>
-                    <Card.Title>Hola</Card.Title>
-                    <Card.Img 
-                        src={
-                          `https://api.nasa.gov/EPIC/archive/natural/${year}/${month}/${day}/${format}/${item.image}.${format}${apiKey}`}
-                    >
-                    </Card.Img>
-                    <p>Year: {year} - Month: {month} - Day {day} - Format {format} <br/> Image {item.image} </p>
-                </Card.Body>
-                </Card>
-            </Container>
-          )
-      })
-
-      }
+        <p>{startDate.toISOString().split("T")[0]}</p>
+      <Container>
+        <Card>
+          <Card.Body>
+            <Card.Title></Card.Title>
+            <Card.Img src={`https://api.nasa.gov/EPIC/archive/natural/${year}/${month}/${day}/${format}/${itemsImg[0].image}.${format}?api_key=DEMO_KEY`}></Card.Img>
+          </Card.Body>
+        </Card>
+      </Container>
     </>
   );
 }
